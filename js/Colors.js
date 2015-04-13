72c6fb37cc71a26d0b7c3819fd110592b51d5640
Eduardo Rabelo <oieduardorabelo@gmail.com>
2015-04-13 23:04:38 +1200

    Colors main component

--- js/Colors.js
+++ js/Colors.js
@@ -1,11 +1,51 @@
 'use strict'
 
 import React from 'react'
+import ColorSlider from './ColorSlider'
 
 class Colors extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      red: 50,
+      green: 50,
+      blue: 50
+    }
+  }
+
+  update() {
+    this.setState({
+      red: this.refs.red.refs.colorSlider.getDOMNode().value,
+      green: this.refs.green.refs.colorSlider.getDOMNode().value,
+      blue: this.refs.blue.refs.colorSlider.getDOMNode().value
+    })
+  }
+
   render() {
+    var bgMix = {
+      backgroundColor: `rgb(${this.state.red} ,${this.state.green} ,${this.state.blue})`
+    }
+    var bgR = {
+      backgroundColor: `rgb(${this.state.red} ,0,0)`
+    }
+    var bgG = {
+      backgroundColor: `rgb(0,${this.state.green} ,0)`
+    }
+    var bgB = {
+      backgroundColor: `rgb(0,0,${this.state.blue})`
+    }
     return (
-      <div>Live udpate React</div>
+      <div>
+        <ColorSlider ref="red" initialValue={this.state.red} update={this.update.bind(this)} />
+        <ColorSlider ref="green" initialValue={this.state.green} update={this.update.bind(this)} />
+        <ColorSlider ref="blue" initialValue={this.state.blue} update={this.update.bind(this)} />
+
+        <div className="bg bg-mix" style={bgMix}></div>
+        <div className="bg bg-parts bg-r" style={bgR}></div>
+        <div className="bg bg-parts bg-g" style={bgG}></div>
+        <div className="bg bg-parts bg-b" style={bgB}></div>
+      </div>
     )
   }
 }
